2ff23a135184e3d2f5135ec1d90dd5a47dfa640b
Pankaj Kumar <madhuban541@gmail.com>
2022-10-27 20:28:56 +0530

    added the signup functionality with email verification

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose")
 const bcrypt= require("bcrypt")
+const saltRounds=10;
 const userSchema=new mongoose.Schema({
     username:{
         type:String,
@@ -27,4 +28,16 @@ const userSchema=new mongoose.Schema({
 
 })
 
+userSchema.pre("save", async function(next){
+    const user=this;
+    if(!user.isModified("password")){
+        console.log("just before hashing password",user.password);
+        return next()
+    }
+
+    user.password=await bcrypt.hash(user.password, saltRounds)
+    console.log("just after hashing password", user.password)
+    next()
+
+})
 module.exports=mongoose.model("user",userSchema)
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,13 +3,14 @@ const router=express.Router()
 const mongoose=require("mongoose")
 const User=require("../models/User.model")
 const jwt=require("jsonwebtoken")
+const nodemailer=require("nodemailer")
 require("dotenv").config()
 
 
 async function mailer(recievermail, code){
 
     let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
+        host: "smtp.gmail.com",
         port: 587,
         secure: false, // true for 465, false for other ports
         requireTLS:true,
@@ -20,7 +21,7 @@ async function mailer(recievermail, code){
       });
 
       let info = await transporter.sendMail({
-        from: 'pk', // sender address
+        from: 'pk@gmail.com', // sender address
         to: recievermail, // list of receivers
         subject: "Email Verification", // Subject line
         text: `Your verificaton code is ${code}`, // plain text body
@@ -28,6 +29,7 @@ async function mailer(recievermail, code){
       });
     
        console.log("Message sent: %s", info.messageId);
+       console.log("Preview URL:%s",nodemailer.getTestMessageUrl(info))
     
 
 }
@@ -37,14 +39,15 @@ router.post("/verify", async(req, res)=>{
         return res.status(422).json({error:"Please provide a valid email address"})
     }else{
       const savedUser=await  User.findOne({email:email})
+      console.log("saveduser",savedUser)
       
       if(savedUser){
-        return res.status(422).json({error:"Invalid credentials"})
+        return res.status(422).json({error:"Invalid Credentials"})
       }
       try {
         let verificationCode= Math.floor(100000+Math.random()*900000)
         await mailer(email, verificationCode)
-        return res.status(200).json({message:"Email sent",email,verificationCode})
+        return res.status(200).json({message:"Verification Code sent to your email",email,verificationCode})
 
         
       } catch (error) {
@@ -52,6 +55,42 @@ router.post("/verify", async(req, res)=>{
       }
     }
 
+})
+
+
+router.post("/changeUsername", async( req, res)=>{
+    const {email,username}=req.body
+    const savedUser=await User.find({username})
+
+    if(savedUser.length>0){
+        return res.status(400).json({error:"Username already exists"})
+    }else{
+        return res.status(200).json({message:"Username available",username,email})
+    }
+})
+
+router.post("/signup",async( req, res)=>{
+    const {username,password,email}=req.body 
+    if(!username||!email||!password){
+        return res.status(400).json({error:"Please add all the fields"})
+    }else{
+        const user=new User({
+            username,
+            email,
+            password
+        })
+        try {
+           await user.save()
+           const token= jwt.sign({_id:user._id},process.env.JWT_SECRET)
+           return res.status(200).json({message:"User Registered successfully",token})
+            
+        } catch (error) {
+            console.log("error",error)
+            return res.status(400).json({message:"User not Registered"})
+
+        }
+    }
+     
 })
 
 
